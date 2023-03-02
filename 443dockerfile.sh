1f470f6a27b06c8aba851a611a35b5580625ac02
VŨ VĂN THÁI <64297334+AZZ-vopp@users.noreply.github.com>
2023-03-02 23:44:05 +0700

    Update 443dockerfile.sh

--- 443dockerfile.sh
+++ 443dockerfile.sh
@@ -114,8 +114,6 @@ pre_install_docker_compose() {
     read -p "Nhập Node ID port 443 :" node_443
     echo -e "Node_80 là : ${node_443}"
 
-    read -p "giới hạn thiết bị :" DeviceLimit
-    echo -e "DeviceLimit = ${DeviceLimit}"
 
     read -p "Nhập subdomain hoặc ip vps vpn cho port443:" CertDomain443
     echo -e "CertDomain port 443 là = ${CertDomain}"
@@ -187,7 +185,7 @@ Nodes:
       EnableVless: false # Enable Vless for V2ray Type
       EnableXTLS: false # Enable XTLS for V2ray and Trojan
       SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: $DeviceLimit # Local settings will replace remote settings, 0 means disable
+      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
       RuleListPath: # ./rulelist Path to local arulelist file
     ControllerConfig:
       ListenIP: 0.0.0.0 # IP address you want to listen
