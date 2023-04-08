8c5ea6516386d9a97cd5cec6d3a806d0fd03a208
VŨ VĂN THÁI <64297334+AZZ-vopp@users.noreply.github.com>
2023-04-08 23:13:50 +0700

    Update lo4.sh

--- lo4.sh
+++ lo4.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Block Censys IP ranges
-iptables -A INPUT -s 162.142.125.0/24 -j DROP
-iptables -A INPUT -s 167.94.138.0/24 -j DROP
-iptables -A INPUT -s 167.94.145.0/24 -j DROP
-iptables -A INPUT -s 167.94.146.0/24 -j DROP
-iptables -A INPUT -s 167.248.133.0/24 -j DROP
-iptables -A INPUT -s 192.35.168.0/23 -j DROP
-ip6tables -A INPUT -s 2620:96:e000:b0cc:e::/64 -j DROP
+firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="162.142.125.0/24" reject'
+firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="167.94.138.0/24" reject'
+firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="167.94.145.0/24" reject'
+firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="167.94.146.0/24" reject'
+firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="167.248.133.0/24" reject'
+firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="192.35.168.0/23" reject'
+firewall-cmd --permanent --add-rich-rule='rule family="ipv6" source address="2620:96:e000:b0cc:e::/64" reject'
 
-# Save the firewall rules
-iptables-save > /etc/iptables/rules.v4
-ip6tables-save > /etc/iptables/rules.v6
+# Reload the firewall
+firewall-cmd --reload
